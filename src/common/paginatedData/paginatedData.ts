1076a241498c272d363ec00b7f9fa2ffd88450a4
Frédéric Woelffel <woelffel.f@gmail.com>
2018-01-18 22:24:32 +0100

    test: Add a spec file for the paginated data wrapper

--- src/common/paginatedData/paginatedData.ts
+++ src/common/paginatedData/paginatedData.ts
@@ -1,3 +1,5 @@
+
+
 export class PaginatedData<T = any> {
 
   constructor(
---
+++ src/common/paginatedData/paginatedDate.spec.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { PaginatedData } from "./paginatedData";
+
+describe('PaginatedData', () => {
+
+  describe('constructor', () => {
+
+    context('when no pagination args are given', () => {
+
+      it('should have a total value equal to data length when no pagination args are given', () => {
+        const p = new PaginatedData(['data01', 'data02']);
+        expect(p.total).to.equal(2);
+      });
+
+    });
+
+  });
+
+});

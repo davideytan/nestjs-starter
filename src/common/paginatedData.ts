964b27e4fc9d924ec24b4b67dea34b9085445727
Frédéric Woelffel <woelffel.f@gmail.com>
2018-01-18 18:04:48 +0100

    feat: Add a paginated data wrapper

---
+++ src/common/paginatedData.ts
@@ -0,0 +1,10 @@
+export class PaginatedData<T = any> {
+
+  constructor(
+    readonly data: T[],
+    readonly offset: number = 0,
+    readonly limit: number = 0,
+    readonly total: number = data.length
+  ) {}
+
+}

4b5dba13f0653045a320d74946171e5a48f40987
Frédéric Woelffel <woelffel.f@gmail.com>
2018-03-01 22:07:49 +0100

    chore: Configure precommit test check

--- src/modules/todo/todo.service.spec.ts
+++ src/modules/todo/todo.service.spec.ts
@@ -1,13 +1,19 @@
 import { Test } from '@nestjs/testing';
 import { TestingModule } from '@nestjs/testing/testing-module';
+import { getRepositoryToken} from '@nestjs/typeorm/typeorm.utils';
 import { TodoService } from './todo.service';
 import { expect } from 'chai';
+import { TodoEntity } from './todo.entity';
 
 describe('TodoService', () => {
   let module: TestingModule;
   beforeEach(() => {
     return Test.createTestingModule({
       components: [
+        {
+          provide: getRepositoryToken(TodoEntity),
+          useValue: {}
+        },
         TodoService
       ]
     }).compile()
